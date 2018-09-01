65973a17b5db42bffc1e8aad89045b75e6aedf2e
WeiJietao <13415156317@163.com>
2018-09-01 23:59:57 +0800

    redux数据外层提取

--- src/million-words/actions/index.js
+++ src/million-words/actions/index.js
@@ -1,19 +1,14 @@
 import Public from './Public.js';
-import Home from './Home.js';
-import Setting from './Setting.js';
-import Clock from './Clock.js';
-import Account from './Account.js';
-import OwnMsg from './OwnMsg.js';
 
-const actions = {};
+const config = require('./../data.json');
+const actions = {
+    ...Public
+};
 
-Object.assign(actions, {
-    ...Public,
-    ...Home,
-    ...Setting,
-    ...Clock,
-    ...Account,
-    ...OwnMsg
+config.menus.forEach((item) => {
+    Object.assign(actions, {
+        ...(require('./' + item.name + '.js').default)
+    });
 });
 
 export default actions;
--- src/million-words/reducers/index.js
+++ src/million-words/reducers/index.js
@@ -1,19 +1,15 @@
 import { combineReducers } from 'redux';
 import Public from './Public.js';
-import Home from './Home.js';
-import Setting from './Setting.js';
-import Clock from './Clock.js';
-import Account from './Account.js';
-import OwnMsg from './OwnMsg.js';
 
-// 使用redux的combineReducers方法将所有reducer打包起来
-const rootReducer = combineReducers({
-    Public,
-    Home,
-    Setting,
-    Clock,
-    Account,
-    OwnMsg
+const config = require('./../data.json');
+const reducer = {
+    Public
+}
+
+config.menus.forEach((item) => {
+    const { name } = item;
+    reducer[name] = require('./' + name + '.js').default;
 });
 
-export default rootReducer;
+// 使用redux的combineReducers方法将所有reducer打包起来
+export default combineReducers(reducer);
