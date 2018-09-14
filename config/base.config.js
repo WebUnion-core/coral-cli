354d13a56c22950d418314eec702abd5c4ee65ae
WeiJietao <13415156317@163.com>
2018-09-14 16:36:27 +0800

    抽取webpack基本配置

---
+++ config/base.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const MODE = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'none';
+const isKonwnMode = ['production', 'development'].join(',').indexOf(MODE) >= 0;
+
+// 设置打包基本配置
+module.exports = {
+    mode: isKonwnMode ? MODE : 'none',
+    entry: {},
+    module: {
+        rules: [
+            {
+                // 脚本打包
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader?cacheDirectory=true',
+                exclude: /node_modules/
+            },
+            {
+                // CSS样式表打包
+                test: /\.(css|scss)$/,
+                use: [
+                    // CSS抽取
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                // 图像打包
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: 'url-loader?limit=8192'
+            },
+            {
+                // 字体、svg等打包
+                test: /\.(woff|ttf|tff|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    // 直接指明第三方模块的绝对路径，减少查找
+    resolve: {
+        alias: {},
+
+        // 最常匹配的放在最前面，减少查找
+        extensions: ['.jsx', '.js', '.scss', '.css', '.png', '.jpg'],
+        modules: [
+            path.resolve(__dirname, './node_modules')
+        ]
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                // 抽离第三方插件
+                vendor: {
+                    test: /node_modules/, // 指定是node_modules下的第三方包
+                    chunks: 'initial',
+                    name: 'vendor' // 打包后的文件名，任意命名
+                    // priority: 10 // 设置优先级，防覆盖
+                }
+            }
+        },
+
+        minimizer: [
+            // JS压缩(混淆)
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+
+            // CSS压缩(混淆)
+            new OptimizeCSSAssetsPlugin()
+        ]
+    },
+    plugins: [
+        // 注入常量
+        new webpack.DefinePlugin({
+            __DEV__: MODE === 'development'
+        })
+    ]
+}
